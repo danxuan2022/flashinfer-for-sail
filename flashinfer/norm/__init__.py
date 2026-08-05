77e313b0f5bb11dd2f432ba919b4c5d9d7de19a3
leonardozcm <leonardo1997zcm@gmail.com>
2026-08-05 15:39:17 +0800

    fix(cute-dsl): flashinfer use cuda implementation by defeault

--- flashinfer/norm/__init__.py
+++ flashinfer/norm/__init__.py
@@ -56,7 +56,7 @@ from ..jit.norm import gen_norm_module
 
 # Use CUDA JIT implementation instead of CuTe DSL (for debugging/fallback)
 # Also fallback to CUDA JIT if nvidia-cutlass-dsl is not installed
-_USE_CUDA_NORM = os.environ.get("FLASHINFER_USE_CUDA_NORM", "0") == "1"
+_USE_CUDA_NORM = os.environ.get("FLASHINFER_USE_CUDA_NORM", "1") == "1"
 
 if not _USE_CUDA_NORM:
     try:
